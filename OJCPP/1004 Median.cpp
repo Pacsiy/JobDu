2267b6afcd84acc8a9cf83204f4d1d66b9bf0816
Pacsiy <1329284393@qq.com>
2017-04-24 22:58:04 +0800

    1004 Median

---
+++ OJCPP/1004 Median.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Administrator on 2017/4/24.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int n,m;
+long long a[1000005];
+long long b[1000005];
+
+int main()
+{
+    while(~scanf("%d",&n))
+    {
+        for(int i=0;i<n;i++)
+            scanf("%lld",&a[i]);
+        scanf("%d",&m);
+        for(int i=0;i<m;i++)
+            scanf("%lld",&b[i]);
+
+        long long ans;
+        int p=0,q=0;
+        for(int i=1;i<=(n+m-1)/2;i++)
+        {
+            if(p==n) q++;
+            else if(q==m) p++;
+            else if(a[p]<b[q]) p++;
+            else q++;
+        }
+        if(p==n) ans=b[q];
+        else if(q==m) ans=a[p];
+        else ans=min(a[p],b[q]);
+        printf("%lld\n",ans);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -9,16 +9,5 @@ using namespace std;
 
 int main()
 {
-    double P,T,G1,G2,G3,GJ,ans;
-    while(~scanf("%lf %lf %lf %lf %lf %lf",&P,&T,&G1,&G2,&G3,&GJ))
-    {
-        if(fabs(G1-G2)<=T) ans=(G1+G2)/2;
-        else if(fabs(G3-G1)<=T&&fabs(G3-G2)>T) ans=(G3+G1)/2;
-        else if(fabs(G3-G2)<=T&&fabs(G3-G1)>T) ans=(G3+G2)/2;
-        else if(fabs(G3-G1)<=T&&fabs(G3-G2)<=T) ans=max(G1,max(G2,G3));
-        else ans=GJ;
 
-        printf("%.1lf\n",ans);
-    }
-    return 0;
 }
